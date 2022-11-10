c5c7eec70fd6e0881009f8d4f166f3b7f78b7be3
WKFilipeDavi <filipedavigarcia@hotmail.com>
2022-11-09 22:30:09 -0300

    requisições test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import ListCourses from './components/ListCourses.vue'; 
+
+
 </script>
 
 <template>
+  <listCourses />
+  <nav>
+    <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
+    <router-link to="/contact">Contact</router-link>
+  </nav>
   <main>
     <router-view />
   </main>
---
+++ src/components/ListCourses.vue
@@ -0,0 +1,16 @@
+<script>
+import axios from "axios";
+
+axios
+  .get("https://api.github.com/users/wkfilipedavi")
+  .then((res) => {
+          console.log(res.data)
+  })
+  .catch((error) => {
+          console.log(error);
+  });
+</script>
+
+<template>
+    <h1>Courses List</h1>
+</template>
