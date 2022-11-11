5f813d64bb7da78e7e1ce3bb35cb481b5f737cf9
WKFilipeDavi <filipedavigarcia@hotmail.com>
2022-11-11 20:16:04 -0300

    implementa: estrutura inicial da página modulos

--- src/views/Course.vue
+++ src/views/Course.vue
@@ -1,6 +1,40 @@
 <script>
 export default {
-    props: [],
+    data() {
+        return {
+            value: {},
+        }
+    },
+    mounted() {
+        this.road();
+    },
+    methods: {
+        road() {
+            if(this.$route.params.stack == 'ux') {
+                this.value = [
+                    {id: 1, title:"fundamentos ux", stack:"ux" },
+                    {id: 2, title:"fundamentos ui", stack:"ux 2"},
+                    {id: 3, title:"planos de carreira", stack:"ux 2"}, 
+                    {id: 4, title:"fundamentos ui/ux 2", stack:"ux 2"},  
+                
+                ];
+                console.log('ux');
+                console.log(this.value);
+                //this.value = 'ux';
+            }
+            else if(this.$route.params.stack == 'dfs') {
+                this.value = [
+                    {id: 1, title:"Algoritmos", stack:"dfs" },
+                    {id: 2, title:"HTTP", stack:"dfs"},
+                    {id: 3, title:"GIT", stack:"dfs"}, 
+                    {id: 4, title:"html", stack:"dfs"},  
+                ];
+                console.log('dfs');
+                console.log(this.value);
+                //this.value = 'dfs';
+            }
+        }
+    }
 }
 
 </script>
@@ -8,6 +42,7 @@ export default {
 
 <template>
     <div>
-        <p>Curso id: {{ this.$route.params.id }}</p>
+        <p>Curso id: {{ this.$route.params.stack }}</p>
+        <p>Nosso valor: </p>
     </div>
 </template>
