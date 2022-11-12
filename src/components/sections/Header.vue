91f158fe400f468295a06efa3211c3696e5d7e0b
WKFilipeDavi <filipedavigarcia@hotmail.com>
2022-11-12 00:21:20 -0300

    Update Header.vue and Course.vue

--- src/components/sections/Header.vue
+++ src/components/sections/Header.vue
@@ -1,15 +1,21 @@
 <script>
+    export default {
+        props: []
+    }
 
 </script>
 
 <template>
     <div class="flex justify-between lg:justify-center pt-9 px-9 lg:pt-20 ">
-        <div v-if="this.$route.path == '/trilhas'">
-            <img class="w-11 h-11 lg:w-[98px]  lg:h-[92px] lg:mb-20 " src="../../assets/images/logo.png">
+        <div>
+            voltar
         </div>
         
+        <div>
+            <img class="w-11 h-11 lg:w-[98px]  lg:h-[92px] lg:mb-20 " src="../../assets/images/logo.png">
+        </div>
 
-        <div v-if="this.$route.path == '/trilhas'">
+        <div class="">
             <router-link to="/" class="text-[12px]">
                 <img class="lg:hidden" src="../../assets/images/hamburguer.png">
             </router-link>
