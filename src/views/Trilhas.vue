cc02d7934c62ac5eed11bb7df7027abd4f43e49d
WKFilipeDavi <filipedavigarcia@hotmail.com>
2022-11-14 20:18:05 -0300

    atualiza: gamificação - trilhas - woking

--- src/views/Trilhas.vue
+++ src/views/Trilhas.vue
@@ -1,80 +1,99 @@
 <script>
-    import ListCourses from '../components/ListCourses.vue';
-    import router from '../router';
-    import axios from "axios";
+import ListCourses from '../components/ListCourses.vue';
+import router from '../router';
+import axios from "axios";
 
-    export default {
-        components: {
-          ListCourses
-        },
-        data() {
-            return {
-                coursesBack: {},
-            }
-        },
-        created() {
-            this.getCourses();
-            console.log(this.coursesBack);
-        },
-        methods: {
-            toCourse(stack, title) {
-                //console.log(stack);
-                //this.$router.push(`/course/${stack}`);
-                //this.$router.push({path:`/course/${stack}`, name: 'course', params: {stack: stack} });
-                //this.$router.push({path:`/course/${id}`, name:"course", params: {stack: id, tsc: 'certo'} });
-                this.$router.push({name: 'course', params: {stack:stack, title: title} });
-            },
-            getCourses() {
-                axios
-                    .get("http://localhost:8080/courses")
-                    .then((res) => {
-                        this.coursesBack = res.data;
-                        //console.log(res.data);
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    });
-            }
-        },
+export default {
+  components: {
+    ListCourses
+  },
+  data() {
+    return {
+      coursesBack: {},
+      //User Data
+      userID: null,
+      userProgress: [],
     }
+  },
+  created() {
+    this.getCourses();
+    console.log(this.coursesBack);
+  },
+  mounted() {
+    this.userID = this.$route.params.id;
+    this.getUserData();
+    console.log('User id: ' + this.userID);
+  },
+  methods: {
+    toCourse(stack, title) {
+      this.$router.push({ name: 'course', params: { stack: stack, title: title } });
+    },
+    getCourses() {
+      axios
+        .get("http://localhost:8080/courses")
+        .then((res) => {
+          this.coursesBack = res.data;
+          //console.log(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getUserData() {
+      //console.log("req: " + "http://localhost:8080/user/" + this.userID);
+      axios
+        //.get("http://localhost:8080/user/1")
+        ({
+          method: 'GET',
+          url: `http://localhost:8080/user/${this.userID}`
+        })
+        .then((res) => {
+          //this.coursesBack = res.data;
+          console.log(res.data);
+          this.addToProgress(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    addToProgress(res) {
+      this.userProgress.push(res.progcurso1);
+      this.userProgress.push(res.progcurso2);
+      //console.log('progs: ' + this.userProgress);
+    }
+  },
+}
 </script>
 
 <template>
-    <Header type="menu" logo="true" />
+  <Header type="menu" logo="true" />
 
-    <div class="mt-[23px]">
-        <h2 class="text-title-1 font-bold text-center mb-[23px] inter">Escolha sua trilha</h2>
-
-        <div class="courses-wrap">
-            <ListCourses 
-                v-for="course in coursesBack"
-                :key="course.id"
-                :title="course.title"
-                :percentage="90"
-                class="course"
-                @tocourse="toCourse(course.stack, course.title)"
-            >
-            </ListCourses>
-        </div>
+  <div class="mt-[23px]">
+    <h2 class="text-title-1 font-bold text-center mb-[23px] inter">Escolha sua trilha</h2>
+    <h1>{{this.userProgress}}</h1>
+    <div class="courses-wrap">
+      <ListCourses v-for="(course, index) in coursesBack" :key="course.id" :title="course.title" :percentage="this.userProgress[index]" class="course"
+        @tocourse="toCourse(course.stack, course.title)">
+      </ListCourses>
     </div>
-    
+  </div>
+
 </template>
 
 <style scoped>
-
 .course:nth-child(3n + 3) .course__content .course__content-bg {
-    background-color: #F956CE;
+  background-color: #F956CE;
 }
 
 .course:nth-child(3n + 2) .course__content .course__content-bg {
-    background-color: #00D6AC;
+  background-color: #00D6AC;
 }
 
 .courses-wrap {
-    display: grid;
-    grid-template-columns: auto;
-    justify-content: center;
-    row-gap: 34px;
+  display: grid;
+  grid-template-columns: auto;
+  justify-content: center;
+  row-gap: 34px;
 }
 
 @media screen and (min-width:768px) {
